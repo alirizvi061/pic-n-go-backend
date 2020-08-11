545f65b801dacb20969310b53481d7550a5cb813
Ali Rizvi <alirizvi@Starboys-Mac.local>
2020-08-11 15:36:50 -0700

    login hashing and comparing working

--- controllers/users.js
+++ controllers/users.js
@@ -5,10 +5,37 @@ const User = require("../models/users.js");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+const SECURITY_TOKEN =
+  "7cace5e42d1ee59685b0ad5c998587cb2fea48560951f9d07e868deb93fb80ab7180197a3fede258e10d92e59a46df58336b92f817b6a371b11649a228d27631";
+
 users.use(bodyParser.urlencoded({ extended: true }));
 users.use(express.json());
 
 //USER ROUTES
+users.post("/login", (req, res) => {
+  console.log(req.body.password);
+  User.findOne({ username: req.body.username }, (err, user) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    }
+    if (bcrypt.compareSync(req.body.password, user.password)) {
+      // console.log(req.body.password);
+      console.log(user.password);
+      let securityToken = jwt.sign(
+        { username: user.username },
+        SECURITY_TOKEN,
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({
+        username: user.username,
+        securityToken: securityToken,
+      });
+    } else {
+      res.status(401).json({ message: "username/password not found" });
+    }
+  });
+});
+
 users.post("/", (req, res) => {
   console.log(res);
   console.log(req);
