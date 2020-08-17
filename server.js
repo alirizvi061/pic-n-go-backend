a474f1898804c26fcba25a108bfe1c2ab0f53671
Ali Rizvi <alirizvi@Starboys-Mac.local>
2020-08-17 11:40:35 -0700

    pushing backend to heroku

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
+
+
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", true);
