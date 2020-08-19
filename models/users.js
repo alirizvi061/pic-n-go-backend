ab535891ada4203600e18050e11ea041982467a2
Ali Rizvi <alirizvi@Starboys-Mac.local>
2020-08-18 19:18:08 -0700

    nothing changed

--- models/users.js
+++ models/users.js
@@ -9,3 +9,11 @@ const userSchema = mongoose.Schema({
 });
 
 module.exports = mongoose.model("User", userSchema);
+
+
+//Generally you wanna make user pic list an array of other mongoose models
+// We're gonna need another model called images
+//Each image at minimum will need a URL string and Array of Notes strings
+//UserPicList could be an array of the images but each image is going to be it's own schema
+//LOOK UP SUB DOCUMENTS on GOOGLE
+//LOOK AT CLASS NOTES FOR SCHEMA
