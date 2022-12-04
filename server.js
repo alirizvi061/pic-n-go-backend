c37256f5d45ae78eb90a204a84cd539dfc16d162
Ali Rizvi <alirizvi@Starboys-Mac.local>
2022-12-04 11:31:14 -0800

    Updated db variable to Adaptable.io standards

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3003;
-const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/picngo";
+const DATABASE_URL =
+  process.env.DATABASE_URL || "mongodb://localhost:27017/picngo";
 
 //middleware
 const whitelist = [
@@ -44,7 +44,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 // mongodb database connection
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(DATABASE_URL, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   useFindAndModify: false,
@@ -52,7 +52,7 @@ mongoose.connect(MONGODB_URI, {
 mongoose.connection.on("error", (err) =>
   console.log(err.message + " is Mongod not running?")
 );
-mongoose.connection.on("disconnected", () => console.log("disconnected"));
+mongoose.connection.on("disconnected", () => console.log("mongoose disconnected"));
 mongoose.connection.once("open", () => {
   console.log("connected to mongoose...");
 });
@@ -80,4 +80,4 @@ app.listen(PORT, () => {
   console.log("listening on PORT: ", PORT);
 });
 
-console.log("MongoDB Port: ", MONGODB_URI)
+console.log("MongoDB Port: ", DATABASE_URL)
